c51825d68918d29a39073647acd05fcc02c772c7
Marín Salinas <marin.salinas@bbxmstudios.com>
2016-04-18 12:41:24 -0500

    Adding support to Reader

--- parse.go
+++ parse.go
@@ -2,6 +2,8 @@ package axmlParser
 
 import (
 	"archive/zip"
+	"bytes"
+	"io"
 	"io/ioutil"
 )
 
@@ -50,6 +52,53 @@ func ParseAxml(axmlpath string, listener Listener) (*Parser, error) {
 	if err != nil {
 		return nil, err
 	}
+	parser := New(listener)
+	err = parser.Parse(bs)
+	if err != nil {
+		return nil, err
+	}
+	return parser, nil
+}
+
+func ParseAPK(f io.Reader, listener Listener) (*Parser, error) {
+	buf := new(bytes.Buffer)
+
+	fileSize, err := io.Copy(buf, f)
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := zip.NewReader(bytes.NewReader(buf.Bytes()), fileSize)
+	if err != nil {
+		return nil, err
+	}
+	//defer r.Close()
+
+	var xmlf *zip.File
+
+	for _, f := range r.File {
+		if f.Name != "AndroidManifest.xml" {
+			continue
+		}
+		xmlf = f
+		break
+	}
+
+	if xmlf == nil {
+		return nil, err
+	}
+
+	rc, err := xmlf.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer rc.Close()
+
+	bs, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return nil, err
+	}
+
 	parser := New(listener)
 	err = parser.Parse(bs)
 	if err != nil {
